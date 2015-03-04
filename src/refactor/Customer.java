a7466886dfc84c2f1c0266292639df09ebef61cb
Rich Dammkoehler <rpd@noradltd.com>
2015-03-04 12:31:34 -0500

    refactor: extract all of statement creation from customer and place in StatementCreator; requires exposure of Customer attributes

--- src/refactor/Customer.java
+++ src/refactor/Customer.java
@@ -4,18 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
-  private static final String NEW_LINE = "\n";
-  private static final String TAB = "\t";
 
-  private String name;
-  private List<Rental> rentals = new ArrayList<Rental>();
-  private double totalAmount = 0;
-  private int frequentRenterPoints = 0;
+  String name;
+  List<Rental> rentals = new ArrayList<Rental>();
+  double totalAmount = 0;
+  int frequentRenterPoints = 0;
   
   public Customer(String name) {
     this.name = name;
   }
 
+  public String createStatement() {
+    return new StatementCreator().createStatement(this);
+  }
+  
   public void addRental(Rental rental) {
     rentals.add(rental);
   }
@@ -24,73 +26,7 @@ public class Customer {
     return name;
   }
 
-  public String createStatement() {
-    StringBuilder statement = new StringBuilder();
-    statement.append(createHeader());
-    statement.append(createBody());
-    statement.append(createFooter());
-    return statement.toString();
-  }
-
-  private String createBody() {
-    StringBuilder lineItems = new StringBuilder();
-    for (Rental rental : rentals) {
-      lineItems.append(createLineItem(rental));
-    }
-    return lineItems.toString();
-  }
-
-  private String createLineItem(Rental rental) {
-    StringBuilder lineItem = new StringBuilder();
-    double thisAmount = determineAmountForRental(rental);
-
-    calculateFrequentRenterPoints(rental);
-
-    totalAmount += thisAmount;
-    lineItem.append(TAB).append(rental.getMovie().getTitle()).append(TAB).append(thisAmount).append(NEW_LINE);
-
-    return lineItem.toString();
-  }
-
-  private void calculateFrequentRenterPoints(Rental rental) {
-    frequentRenterPoints++;
-
-    if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-      frequentRenterPoints++;
-  }
-
-  private double determineAmountForRental(Rental rental) {
-    double thisAmount = 0;
-
-    switch (rental.getMovie().getPriceCode()) {
-    case Movie.REGULAR:
-      thisAmount += 2;
-      if (rental.getDaysRented() > 2)
-        thisAmount += (rental.getDaysRented() - 2) * 1.5;
-      break;
-    case Movie.NEW_RELEASE:
-      thisAmount += rental.getDaysRented() * 3;
-      break;
-    case Movie.CHILDRENS:
-      thisAmount += 1.5;
-      if (rental.getDaysRented() > 3)
-        thisAmount += (rental.getDaysRented() - 3) * 1.5;
-      break;
-    }
-    return thisAmount;
-  }
-
-  private String createFooter() {
-    StringBuilder footer = new StringBuilder();
-    footer.append("You owed ").append(totalAmount).append(NEW_LINE);
-    footer.append("You earned ").append(frequentRenterPoints).append(" frequent renter points\n");
-    return footer.toString();
-  }
-
-  private String createHeader() {
-    return "Rental Record for " + getName() + NEW_LINE;
-  }
-
+  
   public Double getOwed() {
     return totalAmount;
   }
