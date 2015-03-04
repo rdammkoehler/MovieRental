12ffda5dc81b28de0cd71a117df27342e12d9b48
Rich Dammkoehler <rpd@noradltd.com>
2015-03-04 14:18:52 -0500

    refactor: moved calculation of charges into FeeProfile obviating RentalChargeCalculator, not complete

--- src/main/java/refactor/FeeProfile.java
+++ src/main/java/refactor/FeeProfile.java
@@ -1,9 +1,31 @@
 package refactor;
 
+import java.math.BigDecimal;
+
 public class FeeProfile {
 
   enum PriceCode {
-    REGULAR, NEW_RELEASE, CHILDRENS
+    REGULAR(BigDecimal.valueOf(2d), BigDecimal.valueOf(1.5d), 2), 
+    NEW_RELEASE(BigDecimal.ZERO, BigDecimal.valueOf(3d), 0), 
+    CHILDRENS(BigDecimal.valueOf(1.5d), BigDecimal.valueOf(1.5d), 3);
+
+    private BigDecimal rentalFee;
+    private BigDecimal lateFee;
+    private Integer lateAfterDays;
+
+    private PriceCode(BigDecimal rentalFee, BigDecimal lateFee, Integer lateAfterDays) {
+      this.rentalFee = rentalFee;
+      this.lateFee = lateFee;
+      this.lateAfterDays = lateAfterDays;
+    }
+
+    public BigDecimal calculateCharges(Integer daysRented) {
+      BigDecimal charge = new BigDecimal(rentalFee.doubleValue());
+      if (daysRented > lateAfterDays) {
+        charge = charge.add(lateFee.multiply(BigDecimal.valueOf(daysRented - lateAfterDays)));
+      }
+      return charge;
+    }
   }
 
   private PriceCode priceCode;
